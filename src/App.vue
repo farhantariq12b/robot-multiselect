a57ca3505c77d56c6e34434abba028d22ab288b0
Farhan Tariq <farhantariq12b@gmail.com>
2024-01-18 14:04:44 +0500

    use multiselect component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <template>
-  
+  <main class="flex items-center justify-center w-screen h-screen bg-gray-200">
+    <section class="w-1/3">
+      <MultiSelect
+        :items="robots"
+        @onSelect="onSelect"
+        @onRemove="onRemove"
+      />
+    </section>
+  </main>
 </template>
 
 <script setup lang="ts">
-</script>
+import { ref } from 'vue'
+import { useRobotsStore } from '@/stores/robots'
 
+import MultiSelect from '@/components/multiSelect.vue'
+
+const { robots } = useRobotsStore()
+
+const onSelect = (item, selectedItems) => {
+  console.log('SelectedItem:', item, '--- SelectedItems:', selectedItems)
+}
+
+const onRemove = (item, selectedItems) => {
+  console.log('RemovedItem:', item, '--- SelectedItems:', selectedItems)
+}
+</script>
---
+++ src/stores/robots.ts
@@ -0,0 +1,16 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useRobotsStore = defineStore('robots', () => {
+  const robots = ref([
+    { id: 'robot_1', name: 'Robot 1' },
+    { id: 'robot_2', name: 'Robot 2' },
+    { id: 'robot_3', name: 'Robot 3' },
+    { id: 'robot_4', name: 'Robot 4' },
+    { id: 'robot_5', name: 'Robot 5' },
+    { id: 'robot_6', name: 'Robot 6' },
+    { id: 'robot_7', name: 'Robot 7' }
+  ])
+
+  return { robots }
+})
