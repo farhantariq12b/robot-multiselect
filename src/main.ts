1765be86321a8b7872a95d1d70ae5896e6e54abf
Farhan Tariq <farhantariq12b@gmail.com>
2024-01-18 13:56:18 +0500

    adds click outside package

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import vClickOutside from 'click-outside-vue3'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(vClickOutside)
 
 app.mount('#app')
